473ba84761b81912530ec8b5a0e791d7339b0716
ayush565 <68837427+ayush565@users.noreply.github.com>
2022-02-17 09:01:57 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+package java_programs;
+
+import java.util.*;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter two numbers: ");
+
+		double num1 = input.nextDouble();
+		double num2 = input.nextDouble();
+
+		System.out.print("Enter an operator (+, -, *, /): ");
+		char operator = input.next().charAt(0);
+		input.close();
+		double result;
+		//Use switch statement to check operator and perform calculation
+		switch(operator)
+		{
+		case '+':
+		result = num1 + num2;
+		break;
+		case '-':
+		result = num1 - num2;
+		break;
+		case '*':
+		result = num1 * num2;
+		break;
+		case '/':
+		result = num1 / num2;
+		break;
+		// Show an error message if wrong operator
+		default:
+		System.out.printf("Error! operator is not correct");
+		return;
+		}
+		//Show the result of  calculation to the user
+		System.out.printf("%.2f %c %.2f = %.2f \n", num1, operator, num2, result);
+		}
+}
